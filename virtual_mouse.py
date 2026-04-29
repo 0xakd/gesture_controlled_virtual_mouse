c7fdfdbe94418714c8781b306e3ecac1f5fdc05c
Ajay <ajay@ADs-MacBook-Air.local>
2026-04-29 18:24:38 +0530

    mediapipe setup and implemented hand tracking

--- virtual_mouse.py
+++ virtual_mouse.py
@@ -1,8 +1,18 @@
 import cv2
+import mediapipe as mp
+
+mpHands = mp.solutions.hands
+hands = mpHands.Hands(
+    static_image_mode = False,
+    model_complexity=1,
+    min_detection_confidence=0.7,
+    min_tracking_confidence=0.7,
+    max_num_hands = 1
+)
 
 def main():
     cap = cv2.VideoCapture(0)
-    
+    draw = mp.solutions.drawing_utils
     try:
         while cap.isOpened():
             ret,frame = cap.read()
@@ -11,7 +21,20 @@ def main():
                 break
             
             frame = cv2.flip(frame, 1)
+            frameRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            processed = hands.process(frameRGB)
             
+            landmarks_list = []
+            
+            if processed.multi_hand_landmarks:
+                hand_landmarks = processed.multi_hand_landmarks[0]
+                draw.draw_landmarks(frame, hand_landmarks, mpHands.HAND_CONNECTIONS)
+                
+                for lm in hand_landmarks.landmark:
+                    landmarks_list.append((lm.x, lm.y))
+            
+                print(landmarks_list)
+
             cv2.imshow('Frame', frame)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
@@ -22,3 +45,5 @@ def main():
     
 if __name__ == '__main__':
     main()
+    
+    
